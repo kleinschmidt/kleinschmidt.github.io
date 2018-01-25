9aa83857e6fbe09a69be1321bbb2784c17c1d751
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-25 14:37:51 +0000

    Deploy kleinschmidt/kleinschmidt.github.io to github.com/kleinschmidt/kleinschmidt.github.io.git:master

--- showhidediv.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(".sliding > h3.slidingtitle").click(function() {
-        $(this).
-            siblings().toggle();
-    });
-    $(".sliding > h3.slidingtitle").siblings().hide();
-});
